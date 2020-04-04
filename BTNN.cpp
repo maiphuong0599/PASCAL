5b677e2ef1e33d15442f9052c43e58111fef0979
abc <you@example.com>
2020-04-04 11:13:34 +0700

    change

--- BTNN.cpp
+++ BTNN.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
-#include<string>
 using namespace std;
+#include<string>
 
 struct thuvien
 {
@@ -14,9 +14,10 @@ void nhap_thong_tin_sach(thuvien& x)
 	fflush(stdin);
 	cout << "Nhap ma so cuon sach: ";
 	cin >> x.ms;
+	cin.ignore();
 	cout << "Nhap ten cuon sach: ";
 	getline(cin, x.tensach);
-	cout << "\nNhap gia cuon sach: ";
+	cout << "Nhap gia cuon sach: ";
 	cin >> x.gia;
 }
 
@@ -24,7 +25,7 @@ void xuat_thong_tin_sach(thuvien x)
 {
 	cout << "\nMa so cuon sach: " << x.ms;
 	cout << "\nTen cuon sach: " << x.tensach;
-	cout << "\nGia cuon sach: " << x.gia;
+	cout << "\nGia cuon sach: " << x.gia << endl;
 }
 
 void nhap_danh_sach(thuvien a[], int n)
@@ -40,7 +41,7 @@ void xuat_danh_sach(thuvien a[], int n)
 {
 	for (int i = 0; i < n; i++)
 	{
-		cout << "\nCuon sach " << i + 1 << endl;
+		cout << "\nCuon sach " << i + 1;
 		xuat_thong_tin_sach(a[i]);
 	}
 }
@@ -61,7 +62,7 @@ void tim_sach_ten_Y( thuvien a[], int n )
 {
 	string tensach_y;
 	rewind(stdin);
-	cout << "\nNhap ten cuon sach can tim";
+	cout << "\nNhap ten cuon sach can tim ";
 	getline(cin,tensach_y);
 	for (int i = 0; i < n; i++)
 	{
@@ -72,32 +73,38 @@ void tim_sach_ten_Y( thuvien a[], int n )
 	}
 }
 
-/*
 void max_book( thuvien a[], int n)
 {
-		double count = 0;
-		double max = a[0].gia;
-		for (int i = 1; i < n; i++)
+	float max = a[0].gia;
+	for (int i = 0; i < n; i++)
+	{
+		if (max < a[i].gia)
 		{
-			if (a[i].gia = max)
-			{
-				xuat_thong_tin_sach(a[i]);
-			}
+			max = a[i].gia;
 		}
+	}
+	cout << "\nNhung cuon sach co gia cao nhat: ";
+	for (int i = 0; i < n; i++)
+	{
+		if (a[i].gia = max)
+		{
+			xuat_thong_tin_sach(a[i]);
+		}
+	}
 }
-*/
+
 int main()
 {
 	thuvien a[100];
 	int n, x;
-	cout << " Nhap so luong cuon sach ";
+	cout << "Nhap so luong cuon sach ";
 	cin >> n;
 	nhap_danh_sach(a, n);
 	xuat_danh_sach(a, n);
-	cout << "Nhap ma so cuon sach can tim ";
+	cout << "\nNhap ma so cuon sach can tim ";
 	cin >> x;
 	tim_sach_ma_X(a, n, x);
 	tim_sach_ten_Y(a, n);
-	//max_book(a, n);
+	max_book(a, n);
 	return 0;	
 }
