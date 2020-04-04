f53e0a03a1ecb6a9445f625f75a989d3e725fe5b
abc <you@example.com>
2020-04-04 13:25:05 +0700

    help

--- BTNN.cpp
+++ BTNN.cpp
@@ -46,8 +46,11 @@ void xuat_danh_sach(thuvien a[], int n)
 	}
 }
 
-void tim_sach_ma_X(thuvien a[], int n, int x)
+void tim_sach_ma_X(thuvien a[], int n)
 {
+	int x;
+	cout << "\nNhap ma so cuon sach can tim ";
+	cin >> x;
 	for (int i = 0; i < n; i++)
 	{
 		if (a[i].ms == x)
@@ -96,15 +99,65 @@ void max_book( thuvien a[], int n)
 int main()
 {
 	thuvien a[100];
-	int n, x;
-	cout << "Nhap so luong cuon sach ";
-	cin >> n;
-	nhap_danh_sach(a, n);
-	xuat_danh_sach(a, n);
-	cout << "\nNhap ma so cuon sach can tim ";
-	cin >> x;
-	tim_sach_ma_X(a, n, x);
-	tim_sach_ten_Y(a, n);
-	max_book(a, n);
-	return 0;	
+	int luachon;
+	do
+	{
+		
+		system("cls");
+		cout << endl << endl;
+		cout << "               +-------------------------------------------------+ \n";
+		cout << "               |          CHUONG TRINH QUAN LY THU VIEN          | \n";
+		cout << "               |-------------------------------------------------| \n";
+		cout << "               |                                                 | \n";
+		cout << "               |  1. Nhap danh sach cuon sach vao thu vien.      | \n";
+		cout << "               |  2. Xuat danh sanh cac cuon sach trong thu vien.| \n";
+		cout << "               |  3. Tim kiem cuon sach theo ma so.              | \n";
+		cout << "               |  4. Tim kiem cuon sach theo ten.                | \n";
+		cout << "               |  5. Xuat cac cuon sach co gia cao nhat.         | \n";
+		cout << "               |  6. Thoat.                                      | \n";
+		cout << "               |                                                 | \n";
+		cout << "               +-------------------------------------------------+ \n";
+
+		cout << "\nMoi chon cong viec tu 1 den 6\n";
+		cin >> luachon;
+
+		switch (luachon)
+		{
+		case 1:
+			int n;
+			system("cls");
+			cout << "Nhap so luong cuon sach ";
+			cin >> n;
+			nhap_danh_sach(a, n);
+			system("pause");
+			break;
+		case 2:
+			system("cls");
+			xuat_danh_sach(a, n);
+			system("pause");
+			break;
+		case 3:
+			system("cls");
+			tim_sach_ma_X(a, n);
+			system("pause");
+			break;
+		case 4:
+			system("cls");
+			tim_sach_ten_Y(a, n);
+			system("pause");
+			break;
+		case 5:
+			system("cls");
+			max_book(a, n);
+			system("pause");
+			break;
+		case 6:
+			break;
+		default:
+			cout << "Ban chon sai roi vui long nhap lai!!! \n";
+			break;
+		}
+	} while (luachon);
+	
+	return 0;
 }
